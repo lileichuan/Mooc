cbaf80480089401790980d02bbc8df8d3c498a07
lilc <506396403@qq.com>
2015-05-12 23:51:36 +0800

    cordovar

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('starter.controllers', [])
+.controller('CoursesCtrl', function($scope, courses,moocService) {
+  $scope.courses = courses.all();
+    console.log('开始');
+    /*
+    moocService.clientActive()
+    .then(function(data){
+        console.log(data);
+      }, function(data){
+        console.log('结束');
+      })
+*/
+  $scope.getItemHeight = function(item, index) {
+    //使索引项平均都有10px高，例如
+    return 280;
+  };
+  $scope.remove = function(course) {
+    Courses.remove(chat);
+  }
+  $scope.gotoCourseDetail = function(courseId) {
+    Courses.remove(chat);
+  }
+})
+.controller('CourseDetailCtrl', function($scope, $stateParams, courses,chapters) {
+  $scope.index = 1;
+  $scope.course = courses.get($stateParams.courseId);
+  $scope.chapters = chapters.all();
+  $scope.navItems = [{title:'简介',index:0},{title:'课时',index:1}];
+  $scope.navViews = [{title:'简介',index:0},{title:'课时',index:1}];
+  $scope.goPage = function(index){
+     $scope.index = index;
+  }
+})
+.controller('LessonCtrl', function($scope, $stateParams, courses,chapters) {
+})
+
